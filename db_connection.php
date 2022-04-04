d78fd0023ad6d4c67858e902fca7e26481917b68
Israel Pérez <pisrael252@gmail.com>
2022-04-04 12:18:42 +0200

    First Commit

---
+++ db_connection.php
@@ -0,0 +1,32 @@
+<?php
+
+    // Creamos la función que permite conectarse a la DB
+    //function ConnectToDB(){
+
+    //    $dbhost = "uocx-icc02-p8.uoclabs.uoc.es/";
+    //    $dbuser = "wordpress23";
+    //    $dbpass = "oq2xKH2bwfXW9";
+    //    $db = "wordpress23";
+
+    //    $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+
+    //    return $conn;
+    //}
+    
+    // Creamos una función para cerrar la conexión con la DB
+    //function CloseDB($conn){
+    //    $conn -> close();
+    //}
+
+
+    // Evitamos el acceso directo al archivo como medida de seguridad
+    defined('_UOC') or exit('Restricted Access');
+
+    $conn = mysqli_connect("localhost","wordpress23","oq2xKH2bwfXW9","wordpress23");
+    // Check connection
+    if (mysqli_connect_errno()){
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+
+    
+?>
---
+++ login/login.php
@@ -0,0 +1,3 @@
+<?php 
+    include('../templates/index/login.php');
+?> 
