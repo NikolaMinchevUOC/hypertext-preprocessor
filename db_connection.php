e5256378b5f4d2ef779786d99e33dfd089c15960
Israel Pérez <pisrael252@gmail.com>
2022-04-04 12:24:16 +0200

    Change Db file

--- db_connection.php
+++ db_connection.php
@@ -22,7 +22,7 @@
     // Evitamos el acceso directo al archivo como medida de seguridad
     defined('_UOC') or exit('Restricted Access');
 
-    $conn = mysqli_connect("localhost","wordpress23","oq2xKH2bwfXW9","wordpress23");
+    $conn = mysqli_connect("uocx23@uocx-icc02-p8.uoclabs.uoc.es","wordpress23","oq2xKH2bwfXW9","wordpress23");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
