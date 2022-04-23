cacaae8da9ff717215e91df878c21fc031949253
NikolaMinchevUOC <nminchev@uoc.edu>
2022-04-23 22:15:17 +0200

    Clases Done

--- templates/panel/clases.php
+++ templates/panel/clases.php
@@ -10,6 +10,65 @@ $query = "SELECT * FROM `class` WHERE 1";
 $result = mysqli_query($conn, $query) or die(mysql_error());
 
 
+function loadTeachers()
+{
+    require('../db_connection.php');
+    global $teacherid;
+    global $classId;
+
+    if ($teacherid == "") {
+        $query = "SELECT id_teacher AS value, CONCAT(name, ' ', surname) AS name FROM teachers t WHERE NOT EXISTS(SELECT 1 FROM class c WHERE t.id_teacher = c.id_teacher)";
+    } else {
+        $query = "SELECT id_teacher AS value, CONCAT(name, ' ', surname) AS name FROM teachers t WHERE NOT EXISTS(SELECT 1 FROM class c WHERE t.id_teacher = c.id_teacher) OR id_teacher = $teacherid";
+    }
+
+    $result = mysqli_query($conn, $query) or die(mysql_error());
+    if (mysqli_num_rows($result) > 0) {
+        echo "<div class='form-floating mb-2'>";
+        echo "<p>Profesor:</p><select name=\"teacherid\" class=\"form-control\">";
+        while ($row = $result->fetch_assoc()) {
+            if (($classId != null) && ($row["value"] == $teacherid)) {
+                $selected = "selected=selected";
+            } else {
+                $selected = "";
+            }
+            echo "<option value=\"" . $row["value"] . "\" " . $selected . ">" . $row["name"] . "</option>";
+        }
+        echo "</select>";
+    } else {
+        echo "No hay profesores disponibles";
+    }
+    echo "</div>";
+}
+
+
+function loadCourses()
+{
+    require('../db_connection.php');
+    global $courseid;
+    global $classId;
+
+    $query = "SELECT id_course AS value, name FROM courses WHERE active = 1";
+    $result = mysqli_query($conn, $query) or die(mysql_error());
+    if (mysqli_num_rows($result) > 0) {
+        echo "<div class='form-floating mb-2'>";
+        echo "<p>Curso:</p><select name=\"courseid\" class=\"form-control\" placeholder=\"Curso\">";
+        while ($row = $result->fetch_assoc()) {
+            if (($classId != null) && ($row["value"] == $courseid)) {
+                $selected = "selected=selected";
+            } else {
+                $selected = "";
+            }
+            echo "<option value=\"" . $row["value"] . "\" " . $selected . ">" . $row["name"] . "</option>";
+        }
+        echo "</select>";
+    } else {
+        echo "No hay cursos activos";
+    }
+    echo "</div>";
+}
+
+
 ?>
 
 <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
@@ -17,7 +76,7 @@ $result = mysqli_query($conn, $query) or die(mysql_error());
         <h1 class="h2">Clases</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
             <div class="btn-group me-2">
-                <a data-toggle='modal' href='#anadir' class='btn btn-sm btn-secondary'>Añadir Curso</a>
+                <a data-toggle='modal' href='#anadir' class='btn btn-sm btn-secondary'>Añadir Clase</a>
             </div>
         </div>
     </div>
@@ -53,6 +112,11 @@ $result = mysqli_query($conn, $query) or die(mysql_error());
                 ?>
 
                         <!-- Modal Editar-->
+
+                        <?php
+                        
+                        
+                        ?>
                         <div class="modal fade" id="editar_<?php echo $row["id_class"]; ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
@@ -66,23 +130,32 @@ $result = mysqli_query($conn, $query) or die(mysql_error());
 
 
                                                 <div class="form-floating mb-2">
-                                                    <input type="text" class="form-control" name="id_teacher" id="id_teacher" placeholder="ID Profesor" value="<?php echo $row['id_teacher']; ?>" required>
-                                                    <label for="floatingInput">ID Profesor</label>
+                                                    <p>Nombre:</p>
+                                                    <input type="text" class="form-control" name="name" id="name" placeholder="Nombre" required>
+
                                                 </div>
+
+                                                <?php loadTeachers(); ?>
+                                                <?php loadCourses(); ?>
+
                                                 <div class="form-floating mb-2">
-                                                    <input type="text" class="form-control" name="id_course" id="id_course" placeholder="ID Curso" value="<?php echo $row['id_course']; ?>" required>
-                                                    <label for="floatingInput">ID Curso</label>
+                                                    <p>Día:</p>
+                                                    <input type="date" class="form-control" name="dia" id="dia" required>
+
                                                 </div>
+
                                                 <div class="form-floating mb-2">
-                                                    <input type="text" class="form-control" name="id_schedule" id="id_schedule" placeholder="ID Schedule" value="<?php echo $row['id_schedule']; ?>" required>
-                                                    <label for="floatingInput">ID Schedule</label>
+                                                    <p>Empieza a las:</p>
+                                                    <input type="time" class="form-control" name="inicio" id="inicio" required>
                                                 </div>
+
                                                 <div class="form-floating mb-2">
-                                                    <input type="tel" class="form-control" name="name" id="name" placeholder="Nombre" value="<?php echo $row['name']; ?>" required>
-                                                    <label for="floatingInput">Nombre</label>
+                                                    <p>Acaba a las:</p>
+                                                    <input type="time" class="form-control" name="fin" id="fin" required>
                                                 </div>
+
                                                 <div class="form-floating mb-2">
-                                                    <input type="text" class="form-control" name="color" id="color" placeholder="Color" value="<?php echo $row['color']; ?>" required>
+                                                    <input type="color" class="form-control" name="color" id="color" value="#ff0000" required>
                                                     <label for="floatingInput">Color</label>
                                                 </div>
                                                 <button class="w-100 btn btn-lg btn-primary" type="submit" name="register">Enviar</button>
@@ -113,25 +186,33 @@ $result = mysqli_query($conn, $query) or die(mysql_error());
 
                                             <form method="POST" action="../panel/clases/anadir.php">
 
+                                                <div class="form-floating mb-2">
+                                                    <p>Nombre:</p>
+                                                    <input type="text" class="form-control" name="name" id="name" placeholder="Nombre" required>
+
+                                                </div>
+
+                                                <?php loadTeachers(); ?>
+                                                <?php loadCourses(); ?>
 
                                                 <div class="form-floating mb-2">
-                                                    <input type="text" class="form-control" name="id_teacher" id="id_teacher" placeholder="ID Profesor" required>
-                                                    <label for="floatingInput">ID Profesor</label>
+                                                    <p>Día:</p>
+                                                    <input type="date" class="form-control" name="dia" id="dia" required>
+
                                                 </div>
+
                                                 <div class="form-floating mb-2">
-                                                    <input type="text" class="form-control" name="id_course" id="id_course" placeholder="ID Curso" required>
-                                                    <label for="floatingInput">ID Curso</label>
+                                                    <p>Empieza a las:</p>
+                                                    <input type="time" class="form-control" name="inicio" id="inicio" required>
                                                 </div>
+
                                                 <div class="form-floating mb-2">
-                                                    <input type="text" class="form-control" name="id_schedule" id="id_schedule" placeholder="ID Schedule" required>
-                                                    <label for="floatingInput">ID Schedule</label>
+                                                    <p>Acaba a las:</p>
+                                                    <input type="time" class="form-control" name="fin" id="fin" required>
                                                 </div>
+
                                                 <div class="form-floating mb-2">
-                                                    <input type="tel" class="form-control" name="name" id="name" placeholder="Nombre" required>
-                                                    <label for="floatingInput">Nombre</label>
-                                                </div>
-                                                <div class="form-floating mb-2">
-                                                    <input type="text" class="form-control" name="color" id="color" placeholder="Color" required>
+                                                    <input type="color" class="form-control" name="color" id="color" value="#ff0000" required>
                                                     <label for="floatingInput">Color</label>
                                                 </div>
                                                 <button class="w-100 btn btn-lg btn-primary" type="submit" name="register">Añadir</button>
