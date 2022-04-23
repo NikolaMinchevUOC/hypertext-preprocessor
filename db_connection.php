a283523f119869e9fe00872134b5b92b92b24cab
NikolaMinchevUOC <nminchev@uoc.edu>
2022-04-23 18:07:43 +0200

    prueba

--- db_connection.php
+++ db_connection.php
@@ -2,13 +2,10 @@
 
     // Evitamos el acceso directo al archivo como medida de seguridad
     defined('_UOC') or exit('Restricted Access');
-    $conn = mysqli_connect("localhost","wordpress23","oq2xKH2bwfXW9","wordpress23");
+    $conn = mysqli_connect("localhost","niko","niko","producto2");
     $conn->set_charset("utf8");
 
     // Comprobar conexión
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
     }
-
-    
-?>
--- panel/dashboardStudent.php
+++ panel/dashboardStudent.php
@@ -2,5 +2,5 @@
 include('../templates/panel/core/top.php');
 include('../templates/panel/studentPanel.php');
 include('../templates/panel/core/bottom.php');
-include('../templates/panel/calendar.php');
+
 echo "<p>USER PANEL</p>";
