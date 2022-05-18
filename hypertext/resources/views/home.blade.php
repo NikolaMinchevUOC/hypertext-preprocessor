ab8d638d3ba4c070b51ec92d0edded0d2d76e8e8
Israel Pérez <pisrael252@gmail.com>
2022-05-18 19:12:17 +0200

    navbar logic system

--- hypertext/resources/views/home.blade.php
+++ hypertext/resources/views/home.blade.php
@@ -2,54 +2,12 @@
 
 @section('title', 'Home')
 
-@section('navbar')
-<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-    <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Company name</a>
-    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">
-    <ul class="navbar-nav px-3">
-      <li class="nav-item text-nowrap">
-        <a class="nav-link" href="{{ route('login.destroy') }}">Sign out</a>
-      </li>
-    </ul>
-  </nav>
-  
-  <div class="container-fluid">
-    <div class="row">
-      <nav class="col-md-2 d-none d-md-block bg-light sidebar">
-        <div class="sidebar-sticky">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">
-                <span data-feather="home"></span>
-                Dashboard <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="file"></span>
-                Exámenes
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
-                <span data-feather="shopping-cart"></span>
-                Algo más
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-@endsection
-
-@section('h2', 'Estudiante')
-
+@section('h1', 'Bienvenido')
 
 @section('content')
 
 <div class="container">
-    <h1 class="text-5xl text-center pt-24">Bienvenido Estudiante</h1>
+    <h1 class="text-5xl text-center pt-24">Bienvenido</h1>
 </div>
-
-
 
 @endsection
